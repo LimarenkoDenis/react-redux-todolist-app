4b4a26804824b909b2073ccae80eccb1d69b9789
Eugene Rodin <eugene.rodin1@gmail.com>
2016-12-26 15:46:34 +0300

    :tada: & init webpack

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# react-redux-todolist-app
+React-Redux-Todolist-App
+========================
+
+*Task conditions and restrictions:*
+- [ ] Create TODO app like _react-mentoring_ example
+- [x] Use webpack and webpack-dev-server
+- [ ] Filter and selected task must be in URL
+- [ ] Add Undo/Redo
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var path = require('path');
+
+var common = {
+	entry: path.resolve(__dirname, 'src/index.ts'),
+	output: {
+		publicPath: '/build/',
+		path: 'build',
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['', '.ts', '.tsx', '.js']
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader'
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: 'style-loader!css-loader'
+			},
+			{
+				test: /\.(son)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader',
+				exclude: 'node_modules'
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: 'file-loader'
+			}
+		]
+	}
+};
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    module.exports = merge(common, {
+        devServer: {
+            inline: true,
+            hot: true,
+			contentBase: '.',
+			historyApiFallback: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('development')
+                }
+            })
+        ]
+    });
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = merge(common, {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(true),
+            new webpack.optimize.DedupePlugin(),
+        ]
+    })
+}
+

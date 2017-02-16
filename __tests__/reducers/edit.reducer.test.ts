9d8d44a65df442ad9e93d997c01e6ed1a6eb8b40
Eugene Rodin <eugene.rodin1@gmail.com>
2017-02-16 21:18:57 +0300

    reducer awesome test

--- __tests__/reducers/edit.reducer.test.ts
+++ __tests__/reducers/edit.reducer.test.ts
@@ -8,25 +8,26 @@ describe('Edit reducer tests', () => {
 		task: { id: 1, title: 'task 1', active: false, description: 'description...' }
 	};
 
+	const actions = [
+		{ type: TaskActions[TaskActions.CANCEL_TASK_EDIT] },
+		{ type: TaskActions[TaskActions.SAVE_TASK], data: { id: 2, title: 'my task', active: false, description: 'lalala' } },
+		{ type: TaskActions[TaskActions.EDIT_TASK], data: { id: 2, title: 'my task', active: false, description: 'lalala' } }
+	];
+
 	it('Should return the initial state', () => {
 		expect(editReducer(initialState, {})).toEqual(initialState);
 	});
 
 	it('Should set edit state to false', () => {
 		expect(
-			editReducer(initialState, {
-				type: TaskActions[TaskActions.CANCEL_TASK_EDIT]
-			})
+			editReducer(initialState, actions[0])
 		).toEqual({
 			active: false,
 			task: null
 		});
 
 		expect(
-			editReducer(initialState, {
-				type: TaskActions[TaskActions.SAVE_TASK],
-				data: { id: 2, title: 'my task', active: false, description: 'lalala' }
-			})
+			editReducer(initialState, actions[1])
 		).toEqual({
 			active: false,
 			task: null
@@ -35,11 +36,17 @@ describe('Edit reducer tests', () => {
 
 	it('Should set edit state with chosen task and display its title in browser url ', () => {
 		expect(
-			editReducer(initialState, {
-				type: TaskActions[TaskActions.EDIT_TASK],
-				data: { id: 2, title: 'my task', active: false, description: 'lalala' }
-			})
+			editReducer(initialState, actions[2])
 		).toEqual({
+			active: true,
+			task: { id: 2, title: 'my task', active: false, description: 'lalala' }
+		});
+	});
+
+	it('Can be used with reduce', () => {
+		const finalState = actions.reduce(editReducer, initialState);
+
+		expect(finalState).toEqual({
 			active: true,
 			task: { id: 2, title: 'my task', active: false, description: 'lalala' }
 		});
